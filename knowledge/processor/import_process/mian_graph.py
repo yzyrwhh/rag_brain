7c592cafddbe3bce72f069f6ac055bd50c2d3a68
WuHaonan <1509432642@qq.com>
2026-08-15 16:31:25 +0800

    fix: 修正字段与异步处理

--- knowledge/processor/import_process/mian_graph.py
+++ knowledge/processor/import_process/mian_graph.py
@@ -1,7 +1,7 @@
 from langgraph.constants import END
 from langgraph.graph import StateGraph
 
-from knowledge.service.task_service import TaskService
+from knowledge.front.service.task_service import TaskService
 from knowledge.processor.import_process.base import setup_logging
 from knowledge.processor.import_process.nodes.bge_embedding_node import BgeEmbeddingNode
 from knowledge.processor.import_process.nodes.md_img import MdImgNode
